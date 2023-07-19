c9d5d453a36cba4a431f98c7c23d5a8004fc8f57
Alexey Nikitin <lexa.nikitin@gmail.com>
2023-07-19 17:00:01 +0300

    feat: добавил модель Card

---
+++ src/models/card.ts
@@ -0,0 +1,39 @@
+import mongoose, {model, Schema, Types} from "mongoose";
+
+interface ICard {
+  name: string;
+  link: string;
+  owner: Schema.Types.ObjectId;
+  likes: Schema.Types.ObjectId[];
+  createdAt: Date;
+}
+
+const cardSchema = new Schema<ICard>(
+  {
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    likes: {
+      type: [Types.ObjectId],
+      default: [],
+      ref: "user",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+);
+export default model<ICard>("card", cardSchema);
