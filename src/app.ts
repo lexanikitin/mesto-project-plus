66006c28bbaf5bb1da04c40fe32c9dbc9c7f525a
Alexey Nikitin <lexa.nikitin@gmail.com>
2023-07-20 13:26:38 +0300

    feat: добавил мидлвар для хардкода id пользователя

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
+import router from "./routes/index";
+import authMiddleware from "./middleware/authMiddleware";
 
 const app = express();
 
-const { PORT = 3000} = process.env;
+const { PORT = 3000 } = process.env;
 
 mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
 
 app.use(express.json());
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+app.use(authMiddleware, router);
+
+app.listen(+PORT, () => console.log(`Server listening on port ${PORT}`));
