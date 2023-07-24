4c662ead740311b95bb1096c1f5b4fb9fd07070b
Alexey Nikitin <lexa.nikitin@gmail.com>
2023-07-24 12:09:52 +0300

    feat: перенес запись message в конструктор предка

--- src/utilities/ErrorWithCode.ts
+++ src/utilities/ErrorWithCode.ts
@@ -1,12 +1,9 @@
 class ErrorWithCode extends Error {
   statusCode: number;
 
-  message: string;
-
   constructor(status: number, message: string) {
-    super();
+    super(message);
     this.statusCode = status;
-    this.message = message;
   }
 
   static badRequest() {
