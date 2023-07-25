1cdfcf39c6795a8e1992e743dbfd133266f0f736
Alexey Nikitin <lexa.nikitin@gmail.com>
2023-07-25 12:00:08 +0300

    feat: 404 на несуществующий адрес

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import usersRouter from "./users";
 import cardsRouter from "./cards";
+import ErrorWithCode from "../utilities/ErrorWithCode";
 
 const router = express.Router();
 
 router.use("/users", usersRouter);
 router.use("/cards", cardsRouter);
+router.use("*", (req, res, next) => next(ErrorWithCode.notFound()));
 
 export default router;
