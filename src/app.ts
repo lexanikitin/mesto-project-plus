05f3b27b00bb57849b86c9e505fca6dd7574791b
Alexey Nikitin <lexa.nikitin@gmail.com>
2023-07-25 15:52:52 +0300

    refactor: роут для логина и регистрации

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 import router from "./routes/index";
 import authMiddleware from "./middleware/auth";
 import errorMiddleware from "./middleware/error";
-import { createUser, login } from "./controllers/auth";
 
 const app = express();
 
@@ -14,8 +13,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
 app.use(express.json());
 
 app.use(authMiddleware);
-app.post("/signin", login);
-app.post("/signup", createUser);
 app.use(router);
 app.use(errorMiddleware);
 
