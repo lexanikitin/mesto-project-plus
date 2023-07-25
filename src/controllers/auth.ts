12dea33511a3b9240ad2d1b80491a136c4f4eea9
Alexey Nikitin <lexa.nikitin@gmail.com>
2023-07-25 15:49:58 +0300

    feat: роут для логина и регистрации

---
+++ src/controllers/auth.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import User from "../models/user";
+import ErrorWithCode from "../utilities/ErrorWithCode";
+
+export const login = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { email, password } = req.body;
+  User.findUserByCredentials(email, password)
+    .then((user) => {
+      res.cookie("mesto-token", jwt.sign({ _id: user._id }, "mesto", { expiresIn: "7d" }));
+      res.send({ message: "Успешный вход в систему" });
+    })
+    .catch(next);
+};
+
+export const createUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { name, about, avatar, email } = req.body;
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => User.create({ name, about, avatar, email, password: hash }))
+    .then((user) => res.status(201).send(user))
+    .catch((error) => {
+      if (error.name === "ValidationError") {
+        next(ErrorWithCode.badRequest());
+      } else {
+        next(error);
+      }
+    });
+};
